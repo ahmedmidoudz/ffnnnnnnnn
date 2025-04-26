9c4becb42d9ec68c0d0017f5c058ddf79d76cf70
ahmedmidoudz <ahmed0792410160@gmail.com>
2025-04-26 23:57:33 +0100

    Add missing files

---
+++ _next/static/chunks/app/settings/page-3451d78d523ca971.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[662],{26518:(e,s,n)=>{Promise.resolve().then(n.bind(n,6725)),Promise.resolve().then(n.bind(n,34405))}},e=>{var s=s=>e(e.s=s);e.O(0,[855,179,339,353,441,517,358],()=>s(26518)),_N_E=e.O()}]);
